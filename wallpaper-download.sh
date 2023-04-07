3ffc52142361d29eb8170179f9bce90d7ec57dc1
Mason-17 <122631948+Mason-17@users.noreply.github.com>
2023-04-06 21:38:05 -0700

    Create wallpaper-download.sh

---
+++ wallpaper-download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user="$USER"
+
+if [ -d "home/$USER/Pictures/Backgrounds" ]
+then
+    echo "Backgrounds exists, updating..."
+else
+    echo "Backgrounds doesn't exist, creating in [username]/Pictures..."
+fi
+
+git clone https://github.com/Mason-17/wallpapers ~/.tmp
+
+mv ~/.tmp/catppuccin ~/Pictures/Backgrounds
+mv ~/.tmp/halo ~/Pictures/Backgrounds
+mv ~/.tmp/landscapes ~/Pictures/Backgrounds
+mv ~/.tmp/mechs ~/Pictures/Backgrounds
+mv ~/.tmp/misc ~/Pictures/Backgrounds
+mv ~/.tmp/sonic ~/Pictures/Backgrounds
+mv ~/.tmp/star-wars ~/Pictures/Backgrounds
+
+if [ -d "/home/$USER/Pictures/Backgrounds/catppuccin" ] 
+then
+    echo "Directory captppuccin exists, finishing up..." 
+else
+    echo "Error: Directory catppuccin does not exist."
+fi
+
+rm -r ~/.tmp
+
+echo "Finished without errors!"
