4e7880e3c27acbb70d68e22b5fdd7c90c795e3d7
Mason-17 <122631948+Mason-17@users.noreply.github.com>
2023-04-07 15:54:34 -0700

    Update wallpaper-download.sh

--- wallpaper-download.sh
+++ wallpaper-download.sh
@@ -6,7 +6,7 @@ if [ -d "/home/$USER/Pictures/Backgrounds" ]
 then
     echo "Backgrounds exists, updating..."
 else
-    echo "Backgrounds doesn't exist, creating in [username]/Pictures..."
+    echo "Backgrounds doesn't exist, creating in $USER/Pictures..."
     mkdir ~/Pictures/Backgrounds
 fi
 
@@ -19,8 +19,16 @@ then
     echo "Directory captppuccin exists, finishing up..." 
 else
     echo "Error: Directory catppuccin does not exist."
+    exit 1
 fi
 
-rm -rf ~.tmp
+rm -rf ~/.tmp
+
+if [ -d "/home/$USER/.tmp" ] then
+    echo "Cleanup successful..."
+else 
+    echo "Error, .tmp still exists, exiting..."
+    exit 1
+fi
 
 echo "Finished without errors!"
