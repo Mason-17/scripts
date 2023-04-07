1f9c97a0a86bd71fe27737c633df06770007f58c
Mason-17 <122631948+Mason-17@users.noreply.github.com>
2023-04-06 22:49:03 -0700

    Update wallpaper-download.sh

--- wallpaper-download.sh
+++ wallpaper-download.sh
@@ -12,13 +12,14 @@ fi
 
 git clone https://github.com/Mason-17/wallpapers ~/.tmp
 
-mv ~/.tmp/catppuccin ~/Pictures/Backgrounds
-mv ~/.tmp/halo ~/Pictures/Backgrounds
-mv ~/.tmp/landscapes ~/Pictures/Backgrounds
-mv ~/.tmp/mechs ~/Pictures/Backgrounds
-mv ~/.tmp/misc ~/Pictures/Backgrounds
-mv ~/.tmp/sonic ~/Pictures/Backgrounds
-mv ~/.tmp/star-wars ~/Pictures/Backgrounds
+mv ~/.tmp/catppuccin ~/Pictures/Backgrounds/
+mv ~/.tmp/halo ~/Pictures/Backgrounds/
+mv ~/.tmp/landscapes ~/Pictures/Backgrounds/
+mv ~/.tmp/mechs ~/Pictures/Backgrounds/
+mv ~/.tmp/misc ~/Pictures/Backgrounds/
+mv ~/.tmp/sonic ~/Pictures/Backgrounds/
+mv ~/.tmp/star-wars ~/Pictures/Backgrounds/
+mv ~/.tmp/various-os ~/Pictures/Backgrounds/
 
 if [ -d "/home/$USER/Pictures/Backgrounds/catppuccin" ] 
 then
